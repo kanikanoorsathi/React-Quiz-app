a8f29b6d6c13cf7e4023eebd982ad1294f44cab6
kanikanoorsathi <kahinurakter504@gmail.com>
2026-03-08 22:47:09 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="navbar bg-base-100 shadow-sm">
+        <a className="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+    </>
+  );
+}
+
+export default App;
